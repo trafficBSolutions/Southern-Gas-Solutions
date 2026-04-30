e4d7d38a3892ae998081d66cabce08e72ce51703
William Rowell <121638756+trafficBSolutions@users.noreply.github.com>
2026-04-30 14:47:27 -0400

    Delete src/page directory

--- src/page/index.js
+++
@@ -1,8 +0,0 @@
-import Home from './home';
-import Services from './Services';
-import About from './About';
-import OurWork from './OurWork';
-import ServiceAreas from './ServiceAreas';
-import Contact from './Contact';
-
-export { Home, Services, About, OurWork, ServiceAreas, Contact };
