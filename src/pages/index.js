110ef0083c59177f457ce7c9527b8af512d76745
William Rowell <121638756+trafficBSolutions@users.noreply.github.com>
2026-05-01 15:21:01 -0400

    Add new components to export from index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,5 +4,14 @@ import About from './About';
 import OurWork from './OurWork';
 import ServiceAreas from './ServiceAreas';
 import Contact from './Contact';
+import Quote from './Quote';
+import Careers from './Careers';
+import AdminLogin from './AdminLogin';
+import AdminDashboard from './AdminDashboard';
+import AdminQuote from './AdminQuote';
+import ApproveQuote from './ApproveQuote';
 
-export { Home, Services, About, OurWork, ServiceAreas, Contact };
+export {
+  Home, Services, About, OurWork, ServiceAreas, Contact, Quote, Careers,
+  AdminLogin, AdminDashboard, AdminQuote, ApproveQuote,
+};
