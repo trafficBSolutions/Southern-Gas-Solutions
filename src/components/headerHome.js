1680fdf223dd518b09d9e5035b200cf45bb9d672
William Rowell <121638756+trafficBSolutions@users.noreply.github.com>
2026-05-01 15:20:07 -0400

    Add hiring banner and careers link to header

--- src/components/headerHome.js
+++ src/components/headerHome.js
@@ -1,17 +1,24 @@
 import React, { useState } from 'react';
 import '../css/header.css';
+import '../css/careers.css';
 import images from '../utils/images';
 import { BsTelephoneForwardFill } from "react-icons/bs";
+import { HiMenu, HiX } from "react-icons/hi";
 function Header() {
     const [isOpen, setIsOpen] = useState(false);
 
     return (
+      <>
+        <div className="hiring-banner">
+          🔥 Southern Gas Solutions is now hiring for gas jobs!
+          <a href="/careers">Apply Today</a>
+        </div>
         <header className="header">
             <a href="/" className="logo">
                 <img src={images['Southern-Gas-Solutions.svg']} alt="Southern Gas Solutions" />
             </a>
             <button className="menu-button" onClick={() => setIsOpen(!isOpen)}>
-                <ion-icon name="menu-outline"></ion-icon>
+                {isOpen ? <HiX /> : <HiMenu />}
             </button>
             <nav className={`nav ${isOpen ? 'open' : ''}`}>
                 <ul className="nav-list">
@@ -29,21 +36,22 @@ function Header() {
                         </ul>
                     </li>
                     <li><a className="main-nav" href="/about">About</a></li>
-                    
                     <li><a className="main-nav" href="/our-work">Our Work</a></li>
                     <li><a className="main-nav" href="/service-areas">Service Areas</a></li>
+                    <li><a className="main-nav" href="/careers">Careers</a></li>
                     <li><a className="main-nav" href="/contact">Contact Us</a></li>
+                    <li><a className="main-nav" href="/admin-login" style={{fontSize:'0.85rem',opacity:0.6}}>🔒 Admin</a></li>
                 </ul>
                 <div className="nav-number">
                     <BsTelephoneForwardFill className="nav-icon"/>
                     <div className="nav-number-icon">
                     <p>Call Now 24/7</p>
-                    
                     <a href="tel:4048623911">(404)862-3911</a>
                     </div>
                 </div>
             </nav>
         </header>
+      </>
     );
 }
 
