24d075bab832adea8e0f0da0cce72606aedce078
William Rowell <121638756+trafficBSolutions@users.noreply.github.com>
2026-05-01 07:44:29 -0400

    Update footer copyright information and styling

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -33,8 +33,9 @@ const Footer = () => (
         </ul>
       </div>
     </div>
-    <div className="footer-bottom">
-      <p>&copy; {new Date().getFullYear()} Southern Gas Solutions. All rights reserved.</p>
+<div className="footer-copyright">
+      <p className="footer-copy-p">&copy; 2026 Statement Marine LLC - 
+         Website Created by <a className="footer-face" href="https://www.material-worx.com/portfolio" target="_blank" rel="noopener noreferrer">MX Systems</a> - All Rights Reserved.</p>
     </div>
   </footer>
 );
