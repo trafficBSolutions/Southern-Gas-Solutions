86067f88dcc9470002ffbebf27f9054d2e24c937
William Rowell <121638756+trafficBSolutions@users.noreply.github.com>
2026-05-21 14:02:28 -0400

    Add AdminInvoice route to App component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import {
   Home, Services, About, OurWork, ServiceAreas, Contact, Quote, Careers,
-  AdminLogin, AdminDashboard, AdminQuote, ApproveQuote, ServiceDetail, NotFound,
+  AdminLogin, AdminDashboard, AdminQuote, AdminInvoice, ApproveQuote, ServiceDetail, NotFound,
 } from './pages/index';
 import { Routes, Route } from 'react-router-dom';
 import './css/global.css';
@@ -24,8 +24,9 @@ function App() {
       <Route path="/admin-login" element={<AdminLogin />} />
       <Route path="/admin-dashboard" element={<AdminDashboard />} />
       <Route path="/admin-quote" element={<AdminQuote />} />
+      <Route path="/admin-invoice" element={<AdminInvoice />} />
       <Route path="/approve-quote/:token" element={<ApproveQuote />} />
-  <Route path="*" element={<NotFound />} />
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 }
